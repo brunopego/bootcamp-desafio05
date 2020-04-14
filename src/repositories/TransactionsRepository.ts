e4195259cda2ce964378495738f0b7591a3e9ef1
Bruno Lacerda <brunolcpg@gmail.com>
2020-04-13 23:14:12 -0300

    Add GET and POST transactions method

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface AllTransactions {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,46 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): AllTransactions {
+    const allTransactions: AllTransactions = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return allTransactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.getTotalIncome();
+    const outcome = this.getTotalOutcome();
+    const total = income - outcome;
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
+  }
+
+  private getTotalIncome(): number {
+    const initialValue = 0;
+    const income = this.transactions.reduce(function red(accum, curr) {
+      return curr.type === 'income' ? accum + curr.value : accum;
+    }, initialValue);
+    return income || 0;
+  }
+
+  private getTotalOutcome(): number {
+    const initialValue = 0;
+    const outcome = this.transactions.reduce(function red(accum, curr) {
+      return curr.type === 'outcome' ? accum + curr.value : accum;
+    }, initialValue);
+    return outcome || 0;
   }
 }
 
